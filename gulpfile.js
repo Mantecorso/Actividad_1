308ac574119c524a7e087de3ac778c01e71e8a7b
Mantecorso <luisjuradoquesada@gmail.com>
2018-05-30 10:38:26 +0200

    rutas correctas backend anf=d frontend

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,21 @@ cleancss = require('gulp-clean-css'),
 rename = require('gulp-rename');
 
 gulp.task('scripts', ()=> {
-    return gulp.src('public/javascripts/**/*.js')
+    return gulp.src('./public/javascripts/**/*.js')
     .pipe(concat('main.js'))
-    .pipe(gulp.dest('public/dist/javascripts'))
+    .pipe(gulp.dest('./public/dist/javascripts'))
     .pipe(rename('main.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('public/dist/javascripts'));
+    .pipe(gulp.dest('./public/dist/javascripts'));
 })
 
 gulp.task('stylesheets', ()=> {
-    return gulp.src('/public/stylesheets/**/*.css')
+    return gulp.src('./public/stylesheets/**/*.css')
     .pipe(concat('main.css'))
-    .pipe(gulp.dest('public/dist/css'))
+    .pipe(gulp.dest('./public/dist/css'))
     .pipe(rename('main.min.css'))
     .pipe(cleancss())
-    .pipe(gulp.dest('public/dist/css'));
+    .pipe(gulp.dest('./public/dist/css'));
 })
 
 gulp.task('default', ['stylesheets', 'scripts']);
@@ -41,13 +41,13 @@ gulp.task('default', ['stylesheets', 'scripts']);
 //    .pipe(gulp.dest('js/app.min.js'))
 //});
 
-gulp.watch('/public/stylesheets/**/*.css').on('change',
-()=>{  return gulp.src('public/stylesheets/*.css')
+gulp.watch('./public/stylesheets/**/*.css').on('change',
+()=>{  return gulp.src('./public/stylesheets/*.css')
  .pipe(concat('main.css'))
- .pipe(gulp.dest('/public/dist/css'))
+ .pipe(gulp.dest('./public/dist/css'))
  .pipe(rename('main.min.css'))
  .pipe(cleancss())
- .pipe(gulp.dest('/public/dist/css'));}
+ .pipe(gulp.dest('./public/dist/css'));}
 );
 
 gulp.watch('/public/javascripts/**/*.js').on('change',

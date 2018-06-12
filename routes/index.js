90dec616aaba58a82a6a6a66e69c60923135483f
Mantecorso <luisjuradoquesada@gmail.com>
2018-06-12 17:54:47 +0200

    registro y login

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/quienessomos', function(req, res, next) {
+  res.render('quienessomos', { title: 'Quienes somos' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+router.get('/registro', function(req, res, next) {
+  res.render('registro', { title: 'Registro' });
+});
+
 module.exports = router;

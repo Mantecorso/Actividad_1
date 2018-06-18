e0cbf7cd09de1d918529968830af0861b3ac39c8
Mantecorso <luisjuradoquesada@gmail.com>
2018-06-18 12:08:32 +0200

    correccion emailconf, funciona y envia emails

--- Configuration/emailconf.js
+++ Configuration/emailconf.js
@@ -2,7 +2,7 @@ const email = require('nodemailer');
 
 let mailer = {};
 
-    mailer.transporter = Nodemailer.createTransport({
+    mailer.transporter = email.createTransport({
         service: 'Gmail',
         auth: {
             user: 'luisjuradoquesada@gmail.com',
